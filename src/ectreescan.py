429826bf9db0833efabd57d69018b1c9f7d48f0e
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-04-20 19:54:17 +0200

    ec-tree 1.0.0 - first version used 'in the wild'

--- src/ectreescan.py
+++ src/ectreescan.py
@@ -13,6 +13,8 @@ import hashlib
 
 #import pyhash
 
+# Constants
+NUM_MIN_SIZE = 1024
 
 def hashafile(str_full_path, algorithm='md5', max_chunks=0):
     if algorithm == 'sha1':
@@ -50,10 +52,12 @@ def time_algos(str_full_path, dic_times=None):
 
 if __name__ == '__main__':
 
-    str_rootdir = r"/home/martin/Desktop/foto"#/Camera Uploads"
-    str_rootdir = r"/home/output/.TMP"  # /Camera Uploads"
+    str_rootdir = r"/home/martin/Desktop/foto" #/Camera Uploads"
+    str_rootdir = r"/run/media/martin/Seagate Backup Plus Drive/Martin/MUH/Pink_Floyd/Discography"
+
 
     fil_ectree = open(str_rootdir+os.sep+'.ectree', 'w')  # Making sure we have write access.
+    fil_longco = open(str_rootdir+os.sep+'collisions_remover.sh', "w")
 
     dic_tree = dict()
     max_chunks = 1
@@ -72,23 +76,79 @@ if __name__ == '__main__':
                 dic_tree[str_hash][str_fullfn] = dict()
                 dic_tree[str_hash][str_fullfn]['last_check'] = datetime.datetime.now().isoformat()
 
-    # Look for short collisions
+    # Look for short, collisions
+    print("Looking through short collisions: {}".format(str_rootdir))
     for key_short in dic_tree.keys():
         if len(dic_tree[key_short].keys()) > 1:
-            print("collision_Short: {}".format(key_short))
+            #print("collision_Short: {}".format(key_short))
             for fil in dic_tree[key_short].keys():
-                #print("  f: {}".format(fil))
-                str_hash_full = hashafile(fil, 'sha1', 0)
-                dic_tree[key_short][fil]['hash_full'] = str_hash_full
+                dic_tree[key_short][fil]['hash_full'] = hashafile(fil, 'sha1', 0)  # Full Hash
                 dic_tree[key_short][fil]['last_check'] = datetime.datetime.now().isoformat()
+                dic_tree[key_short][fil]['size'] = os.stat(fil).st_size
+
 
     # timestamp the .ectree scan
     dic_tree['timestamp'] = datetime.datetime.now().isoformat()
 
-    print("Done in: {} seconds".format((datetime.datetime.now() - dtt_start).total_seconds()))
+    num_run_dur = (datetime.datetime.now() - dtt_start).total_seconds()
 
     json.dump(dic_tree, fil_ectree)
 
     fil_ectree.close()
 
+    #print('Full dic as pretty')
     #pprint.pprint(dic_tree)
+
+    # Look for real (long) Collisions
+    print("Looking through long collisions: {}".format(str_rootdir))
+    dic_long = dict()
+    for key_short in dic_tree.keys():
+        if isinstance(dic_tree[key_short], dict):  # Ignore dic_tree['timestamp'] etc.
+            if len(dic_tree[key_short].keys()) > 1:  # There is a short collision
+                for fil in dic_tree[key_short].keys():
+                    if not dic_tree[key_short][fil]['hash_full'] in dic_long.keys():
+                        ##print("Opening Long list: {}".format(dic_tree[key_short][fil]['hash_full']))
+                        dic_long[dic_tree[key_short][fil]['hash_full']] = list()
+                    if dic_tree[key_short][fil]['size'] > NUM_MIN_SIZE:
+                        ##print(" Add to Long list: {}".format(fil))
+                        # Add (priority, filename, size)
+                        dic_long[dic_tree[key_short][fil]['hash_full']].append([0, fil, dic_tree[key_short][fil]['size']])
+
+    # Try to prioritise which long-collision copy to keep
+    # Filename contains specific strings
+    lst_mad_words = ['copy', 'cpy', 'backup', 'extra', '(1)']
+    for long_colis in dic_long.keys():
+        for colis in dic_long[long_colis]:
+            for str_mad in lst_mad_words:
+                if str_mad in colis[1]:
+                    colis[0] += 1
+    # one is in a subdir, relative to the other
+    for long_colis in dic_long.keys():
+        for colis in dic_long[long_colis]:
+            str_this_dir = colis[1].rsplit(os.sep, 1)[0]
+            for other_colis in dic_long[long_colis]:
+                str_othr_dir = other_colis[1].rsplit(os.sep, 1)[0]
+                if str_this_dir != str_othr_dir:  # Donøt compare to same dir
+                    if str_othr_dir.find(str_this_dir) >= 0:
+                        colis[0] += 1
+                    if str_this_dir.find(str_othr_dir) >= 0:
+                        other_colis[0] += 1
+    # write remove strings
+    lst_rm = list()
+    for long_colis in dic_long.keys():
+        for colis in sorted(dic_long[long_colis])[1:]:  # Spare the first, with lowest priority for deletion
+            str_rm = 'rm "{}"'.format(colis[1])
+            print(str_rm)
+            lst_rm.append(str_rm)
+
+    # Write Longcollision file
+    print("Writing putput files: ")
+    for long_colis in dic_long.keys():
+        fil_longco.write("# Collision : {}\n".format(long_colis))
+        for colis in dic_long[long_colis]:
+            fil_longco.write("# - {}\n".format(colis))
+    for str_rm in lst_rm:
+        fil_longco.write(str_rm+'\n')
+    fil_longco.close()
+
+    print("Done in: {} seconds".format(num_run_dur))
